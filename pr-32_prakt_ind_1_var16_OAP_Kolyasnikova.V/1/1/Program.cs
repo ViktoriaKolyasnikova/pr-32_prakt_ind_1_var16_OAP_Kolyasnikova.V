66b5af6ff0c07f5822dd278369ff30c081bc20f1
ViktoriaKolyasnikova <94283015+ViktoriaKolyasnikova@users.noreply.github.com>
2021-11-14 16:30:26 +0500

    Add files via upload

---
+++ pr-32_prakt_ind_1_var16_OAP_Kolyasnikova.V/1/1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("t.txt");
+            string o;
+            Console.WriteLine("Содержимое тестового файла:");
+            while (sr.EndOfStream != true)
+            {
+                o = sr.ReadLine();
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine();
+
+            var t = File.ReadAllLines("t.txt");
+            Console.WriteLine("Символы в обратном порядке:");
+            foreach (var r in t)
+            {
+                Stack s = new Stack();
+                foreach (var n in r)
+                {
+                    s.Push(n);
+                }
+                var c = s.Count;
+                
+                for (var i = 0; i < c; i++)
+                {
+                    Console.Write(s.Pop());
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
